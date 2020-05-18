de8366a695664939a2b2a8c49f32cceeb6fb6048
Tapan Halani <tapanhalani231@gmail.com>
2020-05-19 00:39:38 +0530

    made some changes.

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /var/nodeapp/
 mkdir -p /var/log/nodeapp-logs
-forever start index.js -l /var/log/nodeapp-logs/output.log
+forever start index.js -o /var/log/nodeapp-logs/output.log -e /var/log/nodeapp-logs/output.log
